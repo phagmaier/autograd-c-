db4e4f712bcf54e886dc2d0360a11217bf7d1764
Parker Hagmaier <parkerhagmaier@gmail.com>
2023-09-27 03:03:31 -0700

    Update myFunctions.cpp

--- myFunctions.cpp
+++ myFunctions.cpp
@@ -26,32 +26,6 @@ Value power(Value& value, const float& power_to) {
     return out;
 }
 
-/*
-//Need to create children for the output value
-Value relu(Value& myVal) {
-    Value out{(myVal.val > 0) ? myVal.val : 0};
-    out.children.insert(&myVal);
-    out.my_back = [&out, &myVal]() {
-        myVal.grad += (myVal.val > 0) ? myVal.val * out.grad : 0;
-    };
-    return out;
-}
- */
-
-//This is fucking up for some reason
-
-/*
-Value tan_h(Value& myVal){
-    float val = myVal.val;
-    float x = std::tanh(val);
-    Value out{x};
-    out.children.insert(&myVal);
-    out.my_back = [&myVal, &x, &out](){
-        myVal.grad += 1- (x * x) * out.grad;
-    };
-    return myVal;
-}
-*/
 
 
 
